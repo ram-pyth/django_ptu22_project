b31b3e2c97dcd1e55e49e05b5c8e498cdacc93d3
Ramunas P <ramunas.platukis@gmail.com>
2024-05-10 14:40:01 +0300

    PASK 2 Author, Book modeliai

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Autorių lentelės klasė
+    reprezentuojanti vieną autorių"""
+    first_name = models.CharField('Vardas', max_length=100)
+    last_name = models.CharField('Pavardė', max_length=100)
+
+    def __str__(self):
+        """Objekto vaizdavimas stringu"""
+        return f'{self.last_name} {self.first_name}'
+
+
+class Book(models.Model):
+    """Viena knyga, autoriai turi ne po vieną
+    todėl čia apsirašysim Foreign Key"""
+    title = models.CharField('Pavadinimas', max_length=150)
+    summary = models.TextField('Aprašymas', max_length=1000)
+    isbn = models.CharField('ISBN', max_length=13)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
