609dad964ee01da6df52b2929cc2ccff875f6787
Ramunas P <ramunas.platukis@gmail.com>
2024-05-10 15:49:34 +0300

    PASK 2 Genre, BookInstance modeliai

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 class Author(models.Model):
@@ -7,9 +8,19 @@ class Author(models.Model):
     first_name = models.CharField('Vardas', max_length=100)
     last_name = models.CharField('Pavardė', max_length=100)
 
+    class Meta:  # globalūs nuostatai lentelei
+        ordering = ('last_name', 'first_name')
+
     def __str__(self):
         """Objekto vaizdavimas stringu"""
         return f'{self.last_name} {self.first_name}'
+
+
+class Genre(models.Model):
+    name = models.CharField("Žanrai", max_length=15, help_text='Įveskite knygos žanrą/us')
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -19,7 +30,33 @@ class Book(models.Model):
     summary = models.TextField('Aprašymas', max_length=1000)
     isbn = models.CharField('ISBN', max_length=13)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.title
 
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    due_back = models.DateField('Bus prieinama', null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    # statusų rinkinys(ne db stulpelis)
+    LOAN_STATUS = (
+        ('a', 'Administruojama'),
+        ('p', 'Paimta'),
+        ('g', 'Galima paimti'),
+        ('r', 'Rezervuota')
+    )
+
+    # pats lentelės stulpelis, kuris naudos statusų rinkinį
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,  # blank - galimas tuščias laukas formoj
+        default='a',
+        help_text='Knygos kopijos statusas'
+    )
+
+    def __str__(self):
+        return f'{self.id} {self.status} {self.due_back} {self.book} {self.book.author}'
