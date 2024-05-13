74613890f49df068692c7449cde5e9eaafc78130
Ramunas P <ramunas.platukis@gmail.com>
2024-05-13 16:57:01 +0300

    PASK 4 countų išvedimai per template

--- django_ptu22_project/settings.py
+++ django_ptu22_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_ptu22_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- libraryapp/views.py
+++ libraryapp/views.py
@@ -8,6 +8,17 @@ def index(request):
     # suskaičiuojam turimus autorius
     num_authors = Author.objects.count()
 
-    context_my = {'num_authors_t': num_authors}
+    # suskaičiuojam turimas knygas ir finzinius egzempliorius
+    num_books = Book.objects.count()
+    num_bookinstances = BookInstance.objects.count()
+
+    # suskaičiuojam laisvus egzempliorius status == g
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+
+    context_my = {'num_authors_t': num_authors,
+                  'num_books_t': num_books,
+                  'num_bookinstances_t': num_bookinstances,
+                  'num_instances_available_t': num_instances_available
+                  }
 
     return render(request, 'index.html', context=context_my)
