9e18996a9d0e3fb1ed28e400763cc9d0239e3383
Ramunas P <ramunas.platukis@gmail.com>
2024-05-13 16:34:17 +0300

    PASK 4 pirmas template ir viewsas

--- libraryapp/views.py
+++ libraryapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Author, Book, BookInstance, Genre
+
 
 def index(request):
-    return HttpResponse("Hello World")
+    # suskaičiuojam turimus autorius
+    num_authors = Author.objects.count()
+
+    context_my = {'num_authors_t': num_authors}
+
+    return render(request, 'index.html', context=context_my)
