6f6d61a05cef0285add4d8216b5b1dc8a116ee40
Ramunas P <ramunas.platukis@gmail.com>
2024-05-16 15:11:46 +0300

    PASK 6 paruošiam configą paveikslėlių saugyklai

--- django_ptu22_project/settings.py
+++ django_ptu22_project/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# medija folderis
+MEDIA_ROOT = Path(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- django_ptu22_project/urls.py
+++ django_ptu22_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('libraryapp.urls')),
     path('', RedirectView.as_view(url='library/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
