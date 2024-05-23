84974277abd8b60e9e82066a8768c4de65b8cb1f
Ramunas P <ramunas.platukis@gmail.com>
2024-05-23 13:57:37 +0300

    PASK 10 profilio atomatinis sukūrimas prisiregistravus naujam, vartotojui

--- libraryapp/apps.py
+++ libraryapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LibraryappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'libraryapp'
+
+    def ready(self):
+        from .signals import create_profile
